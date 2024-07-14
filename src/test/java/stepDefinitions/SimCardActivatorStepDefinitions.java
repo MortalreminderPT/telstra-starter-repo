6f1b79c96326483bcee190057fcbd17fcd79ec48
mortalreminder <me@mortalreminderpt.net>
2024-07-14 22:51:00 +1000

    stage 3: add BDD test for feature sim card activate

--- src/test/java/stepDefinitions/SimCardActivatorStepDefinitions.java
+++ src/test/java/stepDefinitions/SimCardActivatorStepDefinitions.java
@@ -1,11 +1,22 @@
 package stepDefinitions;
 
 import au.com.telstra.simcardactivator.SimCardActivator;
+import au.com.telstra.simcardactivator.dto.BaseResult;
+import au.com.telstra.simcardactivator.dto.SimCardActivateReq;
+import au.com.telstra.simcardactivator.dto.SimCardActivateResp;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 import io.cucumber.spring.CucumberContextConfiguration;
+import org.junit.Assert;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootContextLoader;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ContextConfiguration;
 
 @CucumberContextConfiguration
@@ -14,5 +25,41 @@ import org.springframework.test.context.ContextConfiguration;
 public class SimCardActivatorStepDefinitions {
     @Autowired
     private TestRestTemplate restTemplate;
+    private SimCardActivateReq activateReq;
+    private ResponseEntity<BaseResult<SimCardActivateResp>> activateResp;
 
+    @Given("I have a new SIM card with iccid {string} and email {string}")
+    public void haveANewSimCard(String iccid, String email) {
+        activateReq = new SimCardActivateReq(iccid, email);
+        System.out.println(activateReq);
+    }
+
+    @When("I request to activate the SIM card")
+    public void requestToActivateTheSimCard() {
+        activateResp = restTemplate.exchange(
+                "http://localhost:8080/activate",
+                HttpMethod.POST,
+                new HttpEntity<>(activateReq),
+                new ParameterizedTypeReference<BaseResult<SimCardActivateResp>>() {
+                }
+        );
+    }
+
+    @Then("the sim card should be activated")
+    public void theSimCardShouldBeActivated() {
+        Assert.assertTrue(activateResp.hasBody());
+        BaseResult<SimCardActivateResp> result = activateResp.getBody();
+        Assert.assertEquals(result.getCode(), 200);
+        Assert.assertEquals(result.getMessage(), "Success");
+        Assert.assertTrue(result.getData().getSuccess());
+    }
+
+    @Then("the sim card should not be activated")
+    public void theSimCardShouldNotBeActivated() {
+        Assert.assertTrue(activateResp.hasBody());
+        BaseResult<SimCardActivateResp> result = activateResp.getBody();
+        Assert.assertEquals(result.getCode(), 200);
+        Assert.assertEquals(result.getMessage(), "Success");
+        Assert.assertFalse(result.getData().getSuccess());
+    }
 }
---
+++ src/test/resources/features/sim_card_activator.feature
@@ -0,0 +1,11 @@
+Feature: Can SIM card be activated?
+
+  Scenario: 1255789453849037777 can be activated
+    Given I have a new SIM card with iccid "1255789453849037777" and email "test@gmail.com"
+    When I request to activate the SIM card
+    Then the sim card should be activated
+
+  Scenario: 8944500102198304826 can not be activated
+    Given I have a new SIM card with iccid "8944500102198304826" and email "test@gmail.com"
+    When I request to activate the SIM card
+    Then the sim card should not be activated
